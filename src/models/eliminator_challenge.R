7d98ac969627b159dddbff26260d4ca877728815
Devon Shurick <dshurick@gmail.com>
2018-09-22 15:00:19 -0700

    NFL eliminator challenge script - create functions

--- src/models/eliminator_challenge.R
+++ src/models/eliminator_challenge.R
@@ -1,18 +1,22 @@
 
 library(magrittr)
 library(Rglpk)
+library(googlesheets)
+library(tidyverse)
+
 
 nfl_2018 <- googlesheets::gs_title("NFL 2018 Expected Wins")
 
 exp_wins <- gs_read(
   nfl_2018,
-  ws = 'Season2018',
+  ws = "Season2018",
   col_types = cols(
     .default = col_number(),
     away_team = col_character(),
     home_team = col_character()
   )
 ) %>%
+  dplyr::filter(week > 2) %>%
   dplyr::mutate_at(.vars = vars(ends_with("_prob")), .funs = funs(. / 100)) %>%
   dplyr::mutate(
     away_team = factor(away_team),
@@ -20,49 +24,59 @@ exp_wins <- gs_read(
     week = factor(week)
   )
 
-# exp_wins$WinProb <- predict(fit, exp_wins, type = "response")
-# 
-# googlesheets::gs_edit_cells(nfl_2018,
-#                             ws = "Season2018",
-#                             input = exp_wins %>%
-#                               dplyr::select(WinProb),
-#                             col_names = FALSE,
-#                             anchor = 'G2')
+# picked_teams <- c("1" = "Detroit", "2" = "New Orleans")
 
-# picked_teams <- c("1" = "Buffalo", "2" = "Seattle")
-picked_teams <- c()
 
-X1 <- Matrix::sparse.model.matrix(~ away_team - 1, data = exp_wins)
-X2 <- Matrix::sparse.model.matrix(~ home_team - 1, data = exp_wins)
-colnames(X1) <- NULL
-colnames(X2) <- NULL
+prep_MILP <- function(df, picked_teams = c()) {
+  X_away_teams <-
+    Matrix::sparse.model.matrix(~away_team - 1, data = df)
+  X_home_teams <-
+    Matrix::sparse.model.matrix(~home_team - 1, data = df)
+  colnames(X_away_teams) <- NULL
+  colnames(X_home_teams) <- NULL
 
-week <- Matrix::sparse.model.matrix(~ week - 1, data = exp_wins)
+  X_week <- Matrix::sparse.model.matrix(~week - 1, data = df)
 
-mat <- Matrix::t(cbind(rbind(X1, X2),
-                       rbind(week, week)))
+  Nteams <- ncol(X_away_teams)
+  Nweeks <- ncol(week)
 
-obj <- log(c(exp_wins$sagarin_away_prob, exp_wins$sagarin_home_prob))
+  # mat must be a numeric vector or a (sparse) matrix of constraint coefficients
+  mat <- Matrix::t(cbind(
+    rbind(X_away_teams, X_home_teams),
+    rbind(week, week)
+  ))
+  obj <- log(c(exp_wins$vegas_away_prob, exp_wins$vegas_home_prob))
 
-picked_indx <- which(levels(exp_wins$away_team) %in% picked_teams)
+  picked_indx <- which(levels(exp_wins$away_team) %in% picked_teams)
 
-dir <- c(rep("<=", 32), rep("==", 17))
-dir[picked_indx] <- "=="
+  dir <- c(rep("<=", Nteams), rep("==", Nweeks))
+  dir[picked_indx] <- "=="
 
-rhs <- rep(1, 49)
-rhs[picked_indx] <- 0
+  rhs <- rep(1, length(dir))
+  rhs[picked_indx] <- 0
+
+  types <- rep("I", length(dir))
+
+  return(list(mat = mat, dir = dir, rhs = rhs, types = types))
+}
+
+milp_args <-
+  prep_MILP(exp_wins, picked_teams = c("1" = "Detroit", "2" = "New Orleans"))
+obj <- log(c(exp_wins$vegas_away_prob, exp_wins$vegas_home_prob))
 
 result <- Rglpk_solve_LP(
   obj = obj,
-  mat = mat,
-  dir = dir,
-  rhs = rhs,
-  types = rep("I", 48),
+  mat = milp_args$mat,
+  dir = milp_args$dir,
+  rhs = milp_args$rhs,
+  types = milp_args$types,
   max = TRUE
 )
 
 awayind <- which(result$solution[1:NROW(exp_wins)] == 1)
 homeind <- which(result$solution[-c(1:NROW(exp_wins))] == 1)
 
-exp_wins[awayind,] %>% dplyr::select(week, away_team, home_team, sagarin_away_prob, sagarin_home_prob)
-exp_wins[homeind,] %>% dplyr::select(week, away_team, home_team, sagarin_away_prob, sagarin_home_prob)
+exp_wins[awayind, ] %>% dplyr::select(week, away_team, home_team, vegas_away_prob, vegas_home_prob)
+exp_wins[homeind, ] %>% dplyr::select(week, away_team, home_team, vegas_away_prob, vegas_home_prob)
+
+exp(result$optimum)
